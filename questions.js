f1f5c58da816bfcaed59fca9292e6be8c1fffdae
Nikolasyuri <nikolasyuri@hotmail.com>
2021-05-19 21:38:01 -0300

    Funcionando!

--- questions.js
+++ questions.js
@@ -3,7 +3,7 @@ let questions = [
     {
         numb: 1,
         question: "O que é harmonia?",
-        anwser: "3 ou mais notas tocadas juntas", 
+        answer: "3 ou mais notas tocadas juntas", 
         options: [
             "3 ou mais notas tocadas juntas",
             "2 ou mais notas tocadas juntas",
@@ -14,7 +14,7 @@ let questions = [
     {
         numb: 2,
         question: "O que é melodia?",
-        anwser: "1 nota tocada após a outra", 
+        answer: "1 nota tocada após a outra", 
         options: [
             "3 ou mais notas tocadas juntas",
             "2 ou mais notas tocadas juntas",
@@ -25,7 +25,7 @@ let questions = [
     {
         numb: 3,
         question: "O que é ritmo?",
-        anwser: "É um padrão que se repete, que não precisa ser necessariamente harmônico ou melódico", 
+        answer: "É um padrão que se repete, que não precisa ser necessariamente harmônico ou melódico", 
         options: [
             "É um estilo musical",
             "É uma banda",
@@ -36,7 +36,7 @@ let questions = [
     {
         numb: 4,
         question: "O que é metrônomo?",
-        anwser: "É uma marcação feito por um tipo de relógio para marcar o tempo", 
+        answer: "É uma marcação feito por um tipo de relógio para marcar o tempo", 
         options: [
             "É um estilo musical",
             "É uma marcação feito por um tipo de relógio para marcar o tempo",
@@ -47,7 +47,7 @@ let questions = [
     {
         numb: 5,
         question: "O piano é um instrumento:",
-        anwser: "É uma marcação feito por um tipo de relógio para marcar o tempo", 
+        answer: "De cordas e percussivo", 
         options: [
             "De cordas",
             "Percussivo",
--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const infoBox = document.querySelector(".info-box");
 const sairBtn = infoBox.querySelector(".buttons .quit"); 
 const continuarBtn = infoBox.querySelector(".buttons .restart");
 const quizBox = document.querySelector(".quiz-box");
+const timeCount = quizBox.querySelector(".timer .timer-cou");
+const timeLine = quizBox.querySelector("header .timeline");
+const optionList = document.querySelector(".option-list");
+const allOptions = optionList.children.length;
 
 //Se o botão Iniciar Quiz for clicado
 startBtn.onclick = ()=>{
@@ -20,28 +24,68 @@ continuarBtn.onclick = ()=>{
     quizBox.classList.add("activeQuiz"); //Mostra o quizBox
     showQuestions(0);
     queCounter(1);
+    startTimer(15);
+    startTimerLine(0);
+    nextBtn.style.display = "none";
 }
 
 let queCount = 0;
 let queNumb = 1;
+let counter;
+let counterLine;
+let timeValue = 15;
+let widthValue = 0;
+let userScore = 0;
 
 const nextBtn = quizBox.querySelector(".next-btn");
-//Se o botão Next for pressionado
+const resultBox = document.querySelector(".result-box");
+const restartQuiz = resultBox.querySelector(".buttons .restart");
+const quitQuiz = resultBox.querySelector(".buttons .quit");
+
+//Se o botão Restart for clicado
+restartQuiz.onclick = ()=>{
+    quizBox.classList.add("activeQuiz");
+    resultBox.classList.remove("activeResult");
+    queCount = 0;
+    queNumb = 1;
+    timeValue = 15;
+    widthValue = 0;
+    userScore = 0;
+    showQuestions(queCount);
+    queCounter(queNumb);
+    clearInterval(counter);
+    startTimer(timeValue);
+    clearInterval(counterLine);
+    startTimerLine(widthValue);
+    nextBtn.style.display = "none";
+}
+
+//Se o botão Sair for clicado
+quitQuiz.onclick = ()=>{
+    window.location.reload();
+}
+
+//Se o botão Next for clicado
 nextBtn.onclick = ()=>{
     if(queCount < questions.length - 1){
         queCount++;
         queNumb++;
         showQuestions(queCount);
         queCounter(queNumb);
+        clearInterval(counter);
+        startTimer(timeValue);
+        clearInterval(counterLine);
+        startTimerLine(widthValue);
+        nextBtn.style.display = "none";
     }else{
-        console.log("Ver Resultado")
+        console.log("Questões Respondidas")
+        showResultBox();
     }
 }
 
 //Pegando perguntas e respostas do array
 function showQuestions(index){
     const queText = document.querySelector(".que-text")
-    const optionList = document.querySelector(".option-list")
     let queTag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
     let optionTag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
                     + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
@@ -55,11 +99,93 @@ function showQuestions(index){
     }
 }
 
+let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+//Quando uma opção é clicada
 function optionSelected(answer){
+    clearInterval(counter);
+    clearInterval(counterLine);
     let userAns = answer.textContent;
-    console.log(userAns);
+    let correctAns = questions[queCount].answer;
+    const allOptions = optionList.children.length;
+    if(userAns == correctAns){
+        userScore += 1;
+        console.log(userScore);
+        answer.classList.add("correct");
+        console.log("Resposta Correta");
+        answer.insertAdjacentHTML("beforeend", tickIcon);
+    }else{
+        answer.classList.add("incorrect")
+        console.log("Respota Incorreta")
+        answer.insertAdjacentHTML("beforeend", crossIcon);
+
+        //Se a respota for incorreta automaticamente mostrar a resposta certa
+        for (let i = 0; i < allOptions; i++) {
+            if(optionList.children[i].textContent == correctAns)
+            optionList.children[i].setAttribute("class", "option correct");       
+        }
+    }
+
+    //Uma vez selecionada desabilita todas as opções
+    for (let i = 0; i < allOptions; i++) {
+        optionList.children[i].classList.add("disabled");
+        nextBtn.style.display = "block";
+    }
+    
 }
 
+function showResultBox(){
+    infoBox.classList.remove("acitveInfo"); //Esconde o infoBox
+    quizBox.classList.remove("activeQuiz"); //Esconde o quizBox
+    resultBox.classList.add("activeResult"); //Mostra o resultBox
+    const scoreText = resultBox.querySelector(".score-text");
+    if(userScore > 3){
+        let scoreTag = '<span>Parabéns, você acertou <p>'+ userScore +'</p> de <p>'+ questions.length +'</p> questões.</span>';
+        scoreText.innerHTML = scoreTag;
+    }else if(userScore > 1){
+        let scoreTag = '<span>Infelizmente, você acertou somente <p>'+ userScore +'</p> de <p>'+ questions.length +'</p> questões.</span>';
+        scoreText.innerHTML = scoreTag;
+    }else{
+        let scoreTag = '<span>Infelizmente, você não acertou nenhuma de <p>'+ questions.length +'</p> questões.</span>';
+        scoreText.innerHTML = scoreTag;
+    }
+}
+
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time;
+        time--;
+        if(time < 9){
+            let addZero = timeCount.textContent;
+            timeCount.textContent = "0" + addZero;
+        }
+        if(time < 0){
+            clearInterval(counter);
+            timeCount.textContent = "00";
+            const allOptions = optionList.children.length;
+            let correctAns = questions[queCount].answer;
+            for (i = 0; i < allOptions; i++) {
+                if(optionList.children[i].textContent == correctAns)
+                optionList.children[i].setAttribute("class", "option correct");
+                optionList.children[i].classList.add("disabled"); 
+                nextBtn.style.display = "block";       
+                }
+            }
+        }   
+}
+
+function startTimerLine(time){
+    counterLine = setInterval(timer, 29);
+    function timer(){
+        time += 1;
+        timeLine.style.width = time + "px";
+        if(time > 539){
+            clearInterval(counterLine);
+        }
+    }
+}
 function queCounter(index){
     const bottomQuesCounter = quizBox.querySelector(".total-que");
     let totalQuesCountTag = '<span><p>'+ index +'</p>de<p>'+ questions.length +'</p>Questões</span>';
